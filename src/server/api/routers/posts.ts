0972ebd45575fd989c43de24dcd007904e67b68e
quasiuslikecautious <zquasius@gmail.com>
2023-08-08 10:13:00 -0500

    Next image tag, style up

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -5,7 +5,20 @@ import {
 } from "~/server/api/trpc";
 
 export const postsRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.post.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.prisma.post.findMany({
+      take: 100,
+      include: {
+        author: {
+          select: {
+            id: true,
+            name: true,
+            image: true,
+          },
+        },
+      },
+    });
+
+    return posts;
   }),
 });
